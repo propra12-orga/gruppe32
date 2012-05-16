62cae2dd9a3944a12cf40f4ea4f12555db1e21ba
Anna Zellner <Anna.Zellner@hhu.de>
2012-05-16 13:50:23 +0200

    zack

---
+++ src/Konstanten.java
@@ -0,0 +1,14 @@
+
+public final class Konstanten {
+	public static final int FENSTER_BREITE = 800;
+	public static final int FENSTER_HOEHE = 800;
+	
+	public static final String FELD_FESTE_MAUER = "FESTE_MAUER";
+	public static final String FELD_FREI = "FREI";
+	public static final String FELD_BOMBERMAN = "BOMBERMAN";
+	public static final String FELD_BOMBE = "BOMBE";
+	
+	public static final int FELD_ANZAHL_ZEILEN = 20;
+	public static final int FELD_ANZAHL_SPALTEN = 20;
+			
+}
