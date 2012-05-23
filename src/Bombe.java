4be237f12b969dec8a539f009de0b51c17ba61d1
misterxyz <mister_vladimirov@yahoo.de>
2012-05-23 14:01:53 +0300

    Bombe verbessert

--- src/Bombe.java
+++ src/Bombe.java
@@ -1,104 +1,149 @@
 import java.util.*; // hier waer evtl gut, dass nochma einzugrenzen
 
-public class Bomb
+public class Bomb extends Gegenstand
 {
-    private final static TimePerTick = 1000; // in millisekunden
-    
-    private static Bomb[][] bombTable;
-    private int locX;
-    private int locY;
-    private double delay;
-    private int ticks;
-    private int owner;
-    private Timer fuse;
-    
-    static
-    {
-        bombTable = new Bomb[maxX-minX][maxY-minY];
-    }
-    
-    private boolean explode_child(int x, int y, int Owner)
-    {
-        switch (field[x][y]) // <===== hier brauche ich informationen ueber das spielfeld
-        {
-            case BOMB: 
-                bombTable[x][y].explode(Owner);
-                return true;
-            case DESTRUCTABLE:
-                // wenn zerstoerbar, die entsprechende wand kaputt machen
-                // danach, nicht mehr weiter in diese richtung "explodieren"
-                return false;
-            case POWERUP:
-                // powerup zerst�ren
-                // danach, nicht mehr weiter in diese richtung "explodieren"
-                return false;
-            case PLAYER_1: case PLAYER_2:
-                // dem getroffenen spieler einen lebenspunkt abziehen, dem besitzer
-                // der ersten bombe einen punkt gutschreiben
-                return true;
-            default:
-                return false;
-        }
-    }
-    
-    private void explode(int Owner)
-    {
-        int x = locX;
-        int y = locY;
-        
-        Bomb[x][y] = null;
+/*
+eine bombe legen:
+new Bombe(int locX, int locY, int strength, double delay, int owner)
+* <locX, locY> gibt die Position auf dem Spielfeld an
+* <strength> gibt die Reichweite der Explosion an (Radius)
+* <delay> gibt die Dauer an, bis die Bombe explodiert (derzeit in Sekunden,
+noch nicht final)
+* <owner> gibt an, welchem Spieler die Bombe gehoert (derzeit vom typ int,
+noch nicht final)
 
-        // druckwelle in positive y richtung
-        for (x = locX, y = locY; y < min(locY+strength,maxY); y++)
-        {
-            if (!explode_child(x,y,Owner))
-                break;
-        }
+eine bombe zuenden:
+.explode(int Owner)
+* <Owner> gibt hier an, wem die Punkte für die Explosion angerechnet werden
+sollen
 
-        // druckwelle in negative y richtung
-        for (x = locX, y = locY; y>max(locY-strength,minY); y--)
-        {
-            if (!explode_child(x,y,Owner))
-                break;
-        }
+eine bombe aus dem spiel entfernen (ohne sie vorher zu zuenden):
+.destroy()
+*/
+private final static TimePerTick = 1000; // Animationsgeschwindigkeit in
+Millisekunden?
+private final static ConversionFactor = 1000; // um von Sekunden nach
+Millisekunden umzurechnen
 
-        // druckwelle in positive x richtung
-        for (x = locX, y = locY; x<min(locX+strength,maxX); x++)
-        {
-            if (!explode_child(x,y,Owner))
-                break;
-        }
+private static Bomb[][] bombTable;
 
-        // druckwelle in negative        x richtung
-        for (x = locX, y = locY; x>max(locX-strength,minX); x--)
-        {
-            if (!explode_child(x,y,Owner))
-                break;
-        }
-    }
-    
-    public void run()
-    {
-        ticks--;
-        /*
-        Hier den Kram fuer die Animation rein?
-        */
-        if (ticks<=0)
-        {
-            explode(owner);
-        }
-    }
-    
-    private Bombe(int locX, int locY, int strength, double delay, int owner)
-    {
-        Bomb[locX][locY] = this;
-        this.locX = locX;
-        this.locY = locY;
-        this.strength = strength;
-        this.delay = delay;
-        this.ticks = (int)(delay/TimePerTick);
-        this.owner = owner;
-        this.fuse = new Timer();
-        this.fuse.schedule(this,1000,1000);
-    }
+private double delay;
+private int ticks;
+private int owner;
+private int strength;
+private Timer fuse;
+
+static
+{
+bombTable = new
+Bomb[Konstanten.FELD_ANZAHL_ZEILEN-0][Konstanten.FELD_ANZAHL_SPALTEN-0];
+}
+
+public void destroy()
+{
+// wenn die bombe hochgegangen ist, brennt die lunte net mehr
+if (fuse!=null)
+{
+fuse.cancel()
+fuse = null;
+}
+// bombe aus allen speicherkonstrukten entfernen
+bombTable[this.getX()][this.getY()] = null;
+Spielfeld.feldArray[this.getX()][this.getY()] = Konstante.FELD_FREI;
+// evtl noch pro spieler gespeichert?
+}
+
+private boolean explode_child(int locX, int locY, int Owner)
+{
+switch (Spielfeld.feldArray[locX][locY])
+{
+case Konstante.FELD_BOMBE:
+bombTable[locX][locY].explode(Owner);
+return true;
+/* case FELD_MAUER:
+// wenn zerstoerbar, die entsprechende wand kaputt machen
+// danach, nicht mehr weiter in diese richtung "explodieren"
+return false;
+case POWERUP:
+// powerup zerstören
+// danach, nicht mehr weiter in diese richtung "explodieren"
+return false;
+*/
+case Konstante.FELD_BOMBERMAN: // case PLAYER_2:
+// dem getroffenen spieler einen lebenspunkt abziehen, dem
+besitzer
+// der ersten bombe einen punkt gutschreiben
+return true;
+default:
+return false;
+}
+}
+
+public void explode(int Owner)
+{
+int locX = this.getX();
+int locY = this.getY();
+
+this.destroy();
+
+// druckwelle in positive y richtung
+for (locX = this.getX(), locY = this.getY(); locY <
+min(this.getY()+this.strength,Konstanten.FELD_ANZAHL_SPALTEN); locY++)
+{
+if (!explode_child(locX,locY,Owner))
+break;
+}
+
+// druckwelle in negative y richtung
+for (locX = this.getX(), locY = this.getY();
+locY>max(this.getY()-this.strength,0); locY--)
+{
+if (!explode_child(locX,locY,Owner))
+break;
+}
+
+// druckwelle in positive x richtung
+for (locX = this.getX(), locY = this.getY();
+locX<min(this.getX()+this.strength,Konstanten.FELD_ANZAHL_ZEILEN); locX++)
+{
+if (!explode_child(locX,locY,Owner))
+break;
+}
+
+// druckwelle in negative x richtung
+for (locX = this.getX(), locY = this.getY();
+locX>max(this.getX()-this.strength,0); locX--)
+{
+if (!explode_child(locX,locY,Owner))
+break;
+}
+}
+
+private void run()
+{
+ticks--;
+/*
+Hier den Kram fuer die Animation rein?
+*/
+if (ticks<=0)
+{
+this.explode(owner);
+}
+}
+
+public Bombe(int locX, int locY, int strength, double delay, int owner)
+{
+bombTable[locX][locY] = this;
+Spielfeld.feldArray[locX][locY] = Konstanten.FELD_BOMBE;
+// evtl noch pro spieler speichern?
+
+this.setX(locX);
+this.setY(locY);
+this.strength = strength;
+this.delay = delay;
+this.ticks = (int)(delay*ConversionFactor/TimePerTick);
+this.owner = owner;
+this.fuse = new Timer();
+this.fuse.schedule(this,1000,1000);
+}
 };
