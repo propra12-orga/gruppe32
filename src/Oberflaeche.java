a0932131d905861d4c4b09602d6d466a2ebf05d2
ArchieZ <Anna.Zellner@hhu.de>
2012-06-12 23:26:43 +0300

    Update master

--- src/Oberflaeche.java
+++ src/Oberflaeche.java
@@ -10,24 +10,16 @@ import javax.swing.JLabel;
 import javax.swing.JMenu;
 import javax.swing.JMenuBar;
 import javax.swing.JMenuItem;
-<<<<<<< HEAD
 import javax.swing.JPanel;
-=======
->>>>>>> 7604a93fa1f456f9bb4f4074357bd3658e3df8f0
 import javax.swing.KeyStroke;
 
 import javax.swing.*;
 
-<<<<<<< HEAD
 /**
  * 
  * @author Archie, Thorsten Beckers
  *
  */
-=======
-
-
->>>>>>> 7604a93fa1f456f9bb4f4074357bd3658e3df8f0
 public class Oberflaeche extends JFrame
 {
 	/**
@@ -35,27 +27,20 @@ public class Oberflaeche extends JFrame
 	 */
 	private static final long serialVersionUID = 1L;
 
-	//private JPanel panelSpielfeld;
-
 	private HashMap<String, ImageIcon> imageIconHashMap;
 		
 	public Oberflaeche()
 	{
 		super("Bomberman V1");
 	}
-<<<<<<< HEAD
 	
 	/**
 	 * Images, Icons, Grafiken werden geladen
 	 */
-=======
-
->>>>>>> 7604a93fa1f456f9bb4f4074357bd3658e3df8f0
 	private void FuelleBilderbuch()
 	{
 		this.imageIconHashMap = new HashMap<String, ImageIcon>();
 		
-<<<<<<< HEAD
         this.imageIconHashMap.put(Konstanten.FELD_BOMBE, new ImageIcon("Bilder/Bombe.png"));
 		this.imageIconHashMap.put(Konstanten.FELD_FESTE_MAUER, new ImageIcon("Bilder/unkaputtbar.gif"));
 		this.imageIconHashMap.put(Konstanten.FELD_FREI, new ImageIcon("Bilder/frei.jpg"));
@@ -64,17 +49,8 @@ public class Oberflaeche extends JFrame
 	}
 	
 	/**
-	 * die Oberfl�che wird gezeichnet
+	 * die Oberfläche wird gezeichnet
 	 */
-=======
-                this.imageIconHashMap.put(Konstanten.FELD_BOMBE, new ImageIcon("Bilder/Bombe.png"));
-		this.imageIconHashMap.put(Konstanten.FELD_FESTE_MAUER, new ImageIcon("Bilder/unkaputtbar.gif"));
-		this.imageIconHashMap.put(Konstanten.FELD_FREI, new ImageIcon("Bilder/frei.jpg"));
-		this.imageIconHashMap.put(Konstanten.FELD_BOMBERMAN, new ImageIcon("Bilder" +
-				"/Bomberman.gif"));
-	}
-	
->>>>>>> 7604a93fa1f456f9bb4f4074357bd3658e3df8f0
 	public void Initialisieren()
 	{
 		this.FuelleBilderbuch();
@@ -93,122 +69,7 @@ public class Oberflaeche extends JFrame
 		});
 	}
 
-<<<<<<< HEAD
 	 		private JPanel GetPanel()
-=======
-	
-	private void FinalisiereFrame() {
-		
-		//this.update(getGraphics());
-		this.pack();
-		this.setVisible(true);
-		//this.repaint();
-		}	
-	
-	
-		
-	private JMenuBar ErzeugeMenuBar()
-	{
-
-		JMenuBar menuBar;
-		JMenu menu;
-		JMenu subMenu;
-		JMenu subSubMenu;
-		JMenuItem menuItem;
-
-
-		
-menuBar = new JMenuBar();
-
-menu = new JMenu("Optionen");
-menuBar.add(menu);
-
-
-
-subMenu = new JMenu("New Game");
-
-menuItem = new JMenuItem("1 Player");
-subMenu.add(menuItem);
-
-menuItem = new JMenuItem("2 Players");
-subMenu.add(menuItem);
-
-menuItem = new JMenuItem("3 Players");
-subMenu.add(menuItem);
-
-menuItem = new JMenuItem("4 Players");
-subMenu.add(menuItem);
-
-menu.add(subMenu);
-
-
-
-subMenu = new JMenu("Statistics");
-
-menuItem = new JMenuItem("Level1 high score list");
-subMenu.add(menuItem);
-
-menuItem = new JMenuItem("Level2 high score list");
-subMenu.add(menuItem);
-menu.add(subMenu);
-
-
-
-
-subMenu = new JMenu("Options");
-
-subSubMenu = new JMenu("Levels");
-
-menuItem = new JMenuItem("Level 1");
-subSubMenu.add(menuItem);
-
-menuItem = new JMenuItem("Level 2");
-subSubMenu.add(menuItem);
-
-subMenu.add(subSubMenu);
-menu.add(subMenu);
-
-/* wäre schöner, wenn der Audio Branch
- * ne Checkbox hätte für Sound On/Off
- */
-subSubMenu = new JMenu("Audio");
-
-menuItem = new JMenuItem("Sound Off");
-subSubMenu.add(menuItem);
-
-menuItem = new JMenuItem("Sound On");
-subSubMenu.add(menuItem);
-
-subMenu.add(subSubMenu);
-menu.add(subMenu);
-
-
-menuItem = new JMenuItem("Quit");
-menu.add(menuItem);
-
-
-
-
-menu = new JMenu("Aid");
-menuBar.add(menu);
-
-menuItem = new JMenuItem("Manual");
-menu.add(menuItem);
-
-menuItem = new JMenuItem("Upgrading");
-menu.add(menuItem);
-
-
-return menuBar;
-
-}
-
-
-
-
-	
-		private JPanel GetPanel()
->>>>>>> 7604a93fa1f456f9bb4f4074357bd3658e3df8f0
 	{
 		GridLayout layout = new GridLayout(0, Konstanten.FELD_ANZAHL_SPALTEN);
 		layout.setHgap(1);
@@ -276,7 +137,6 @@ return menuBar;
 
 
 		
-<<<<<<< HEAD
 		menuBar = new JMenuBar();
 
 		menu = new JMenu("Optionen");
@@ -320,25 +180,6 @@ return menuBar;
 					
 				menuItem = new JMenuItem("Level 1");
 				subSubMenu.add(menuItem);
-=======
-		panelMitSpielfeld.removeAll();
-				
-		for (int i = 0; i < Konstanten.FELD_ANZAHL_ZEILEN; i++)
-		{
-			for (int j = 0; j < Konstanten.FELD_ANZAHL_SPALTEN; j++)
-			{
-				ImageIcon aktuellesFeldIcon = new ImageIcon();
-				String schluesselFeldInhalt = spielfeld.feldArray[i][j];
-                                if (Bombe.bombTable[i][j]==null)
-                                {
-                                    aktuellesFeldIcon = this.imageIconHashMap.get(schluesselFeldInhalt);
-                                } else {
-                                    aktuellesFeldIcon = this.imageIconHashMap.get(Konstanten.FELD_BOMBE);
-                                }
-				
-				JLabel anzeigeLabel = new JLabel(aktuellesFeldIcon);
-				panelMitSpielfeld.add(anzeigeLabel);
->>>>>>> 7604a93fa1f456f9bb4f4074357bd3658e3df8f0
 				
 				menuItem = new JMenuItem("Level 2");
 				subSubMenu.add(menuItem);
